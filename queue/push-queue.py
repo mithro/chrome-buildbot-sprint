98c6bc38e329fa32d40ea3ee2cc12ed3464da308
Tim 'mithro' Ansell <mithro@mithis.com>
2015-01-20 16:00:39 +1100

    Multistage pipelined queue.

--- queue/push-queue.py
+++ queue/push-queue.py
@@ -7,26 +7,44 @@
 import simplejson
 import time
 import urllib
+import pprint
 
-snapshots = []
-instances = []
+snapshots = {"src": {}, "build": {}, "test": {}}
+instances = {"src": {}, "build": {}, "test": {}}
 
-def SnapshotExists(commit_id):
-    return commit_id in snapshots
+def SnapshotExists(mtype, commit_id):
+    return commit_id in snapshots[mtype]
 
-def InstanceExists(commit_id):
-    return commit_id in instances
+def InstanceExists(mtype, commit_id):
+    return commit_id in instances[mtype]
 
-def InstanceStart(from_commit, new_commit):
-    print "Launching instance for {0} based of {1}".format(from_commit, new_commit)
-    instances.append(new_commit)
+def InstanceStart(mtype, new_commit, from_snapshot):
+    assert from_snapshot[0] in snapshots
+    assert from_snapshot[1] in snapshots[from_snapshot[0]]
 
-def UpdateState():
-    time.sleep(5)
-    while len(instances) > 0:
-        commit_id = instances.pop(0)
-        print "Completing {0}".format(commit_id)
-        snapshots.append(commit_id)
+    print "Launching {0} for {1} based of {2}".format(mtype, new_commit, from_snapshot)
+    instances[mtype][new_commit] = time.time()
+
+def LaunchInstances(commit_ids, instance_type, base_type):
+    for i, commit_id in enumerate(commit_ids[1:], 1):
+        # If we already exist, then do nothing.
+        if SnapshotExists(instance_type, commit_id):
+            continue
+
+        # If we don't have a snapshot to start from, then just ignore the
+        # commit.
+        previous_commit_id = commit_ids[i-1]
+        if not SnapshotExists(base_type, previous_commit_id):
+            continue
+
+        # If an instance is already trying to create a new snapshot, then
+        # don't do anything.
+        if InstanceExists(instance_type, commit_id):
+            continue
+
+        # Start an instance up on this commit.
+        else:
+            InstanceStart(instance_type, commit_id, from_snapshot=(base_type, previous_commit_id))
 
 
 while True:
@@ -38,28 +56,35 @@ while True:
     commit_ids = [entry['commit'] for entry in data['log']][::-1]
     commit_info = {entry['commit']: entry for entry in data['log']}
 
-    snapshots.append(commit_ids[0])
+    snapshots["src"][commit_ids[0]] = 0
 
     while True:
-        for i, commit_id in enumerate(commit_ids[1:], 1):
+        # The source updater
+        LaunchInstances(commit_ids, "src", "src")
 
-            # If we already exist, then do nothing.
-            if SnapshotExists(commit_id):
-                continue
+        # The builders
+        LaunchInstances(commit_ids, "build", "src")
 
-            # If we don't have a snapshot to start from, then just ignore the
-            # commit.
-            previous_commit_id = commit_ids[i-1]
-            if not SnapshotExists(previous_commit_id):
-                continue
+        # The testers
+        LaunchInstances(commit_ids, "test", "build")
 
-            # If an instance is already trying to create a new snapshot, then
-            # don't do anything.
-            if InstanceExists(commit_id):
-                continue
+        for mtype in instances:
+            if mtype == "src":
+                runtime = 2
+            elif mtype == "build":
+                runtime = 10
+            elif mtype == "test":
+                runtime = 7
 
-            # Start an instance up on this commit.
-            else:
-                InstanceStart(previous_commit_id, commit_id)
+            for commit_id in instances[mtype].keys():
+                if (time.time() - instances[mtype][commit_id]) > runtime:
+                    print "Completing {0} for {1}".format(mtype, commit_id)
+                    snapshots[mtype][commit_id] = time.time()
+                    del instances[mtype][commit_id]
 
-        UpdateState()
+        time.sleep(0.5)
+        print "Running",
+        for mtype in instances:
+            print "%s %i" % (mtype, len(instances[mtype])), 
+        print
+
