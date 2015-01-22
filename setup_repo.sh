853bbb54e874663c30e494d7b49cff211896dc16
Tim 'mithro' Ansell <mithro@mithis.com>
2015-01-22 14:48:48 +1100

    Making the setup script nicer.

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 # Sets up the repo dependencies.
-git clone https://github.com/apache/libcloud.git
-sudo pip install backports.ssl-match-hostname
+if [ ! -d libcloud ]; then
+	git clone https://github.com/apache/libcloud.git
+fi
+
+if python -c "import backports.ssl_match_hostname"; then
+	true
+else
+	sudo pip install backports.ssl-match-hostname
+fi
+if [ ! -e ../gce_bot_rsa ]; then
+	echo "Need ssh key in ../gce_bot_rsa"
+fi
+
+if [ ! -e ../chrome-buildbot-sprint-c514ee5826d1.pem ]; then
+	echo "Need gce API key in ../chrome-buildbot-sprint-c514ee5826d1.pem"
+fi
