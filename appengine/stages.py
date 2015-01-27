f84f1f0fa66e7c94c37ba8ceb62257cd6939f59b
Tim 'mithro' Ansell <mithro@mithis.com>
2015-01-27 15:00:49 +1100

    Making the printing different.

--- appengine/stages.py
+++ appengine/stages.py
@@ -33,7 +33,45 @@ def info(type, value, tb):
 sys.excepthook = info
 
 
+PADDING=60
+END=' | '
+def pretty_print_status(t, skip_tid=False):
+  tid = t.tid
+  if skip_tid:
+    tid = '--->'
 
+  if not skip_tid:
+    padding = ' '*(PADDING - len(tid))
+    print(padding, end='')
+
+    tid = t.tid.split('-')
+    print('-'.join(tid[:5]), end='-')
+    print_color('-'.join(tid[5:]), color='cyan', end=' ')
+    print('   ', end=END)
+  else:
+    print(' '*PADDING, end='')
+    print('\-> ', end=END)
+
+  if t.is_startable():
+    print_color("startable(true) ", color='green', end=END)
+  else:
+    print("startable(false)", end=END)
+
+  if t.is_running():
+    print_color("running(true) ", color='red', end=END)
+  else:
+    print("running(false)", end=END)
+
+  if t.is_finished():
+    print_color("finished(true) ", color='blue', end=END)
+  else:
+    print("finished(false)", end=END)
+
+  print()
+  if isinstance(t, WaitOnOtherTasks):
+    pretty_print_status(t.task_to_run, skip_tid=True)
+  else:
+    print()
 
 
 class Stage(object):
@@ -171,32 +209,36 @@ if __name__ == "__main__":
       old_volumes = []
       old_snapshots = []
       while self.go:
-        nodes = driver.list_nodes()
-        volumes = driver.list_volumes()
-        snapshots = driver.ex_list_snapshots()
+        try:
+          nodes = driver.list_nodes()
+          volumes = driver.list_volumes()
+          snapshots = driver.ex_list_snapshots()
 
-        for node in nodes:
-          if self.skip(node):
-            continue
-          Instance.load(node.name, gce_obj=node)
+          for node in nodes:
+            if self.skip(node):
+              continue
+            Instance.load(node.name, gce_obj=node)
 
-        for volume in volumes:
-          if self.skip(volume):
-            continue
-          Disk.load(volume.name, gce_obj=volume)
+          for volume in volumes:
+            if self.skip(volume):
+              continue
+            Disk.load(volume.name, gce_obj=volume)
 
-        for snapshot in snapshots:
-          if self.skip(snapshot):
-            continue
-          Snapshot.load(snapshot.name, gce_obj=snapshot)
+          for snapshot in snapshots:
+            if self.skip(snapshot):
+              continue
+            Snapshot.load(snapshot.name, gce_obj=snapshot)
 
-        self.ready = True
-        if self.output:
-          if old_nodes != nodes or old_volumes != volumes or old_snapshots != snapshots:
-            print("="*80+'\n', time.time(), "Finish updating gce\n", pprint.pformat(memcache), '\n'+"="*80+'\n')
-          old_nodes = nodes
-          old_volumes = volumes
-          old_snapshots = snapshots
+          self.ready = True
+          if self.output:
+            if old_nodes != nodes or old_volumes != volumes or old_snapshots != snapshots:
+              print("="*80+'\n', time.time(), "Finish updating gce\n", pprint.pformat(memcache), '\n'+"="*80+'\n')
+            old_nodes = nodes
+            old_volumes = volumes
+            old_snapshots = snapshots
+        except Exception, e:
+          print(e)
+          time.sleep(1)
 
   updater = Updater()
   updater.start()
@@ -206,17 +248,12 @@ if __name__ == "__main__":
   print("SyncStage")
   print("-"*80)
   for t in SyncStage(previous_commit, current_commit).tasklets():
-    print(t)
-    print(('startable', t.is_startable()), ('running', t.is_running()), ('done', t.is_finished()))
-    if isinstance(t, WaitOnOtherTasks):
-      print("-->", t.task_to_run)
-      print("-->", ('startable', t.task_to_run.is_startable()), ('running', t.task_to_run.is_running()), ('done', t.task_to_run.is_finished()))
-    print
+    pretty_print_status(t)
 
 
   try:
     updater.output = False
-    raw_input("okay?")
+    raw_input("okay? ")
     updater.output = True
 
     """
@@ -232,29 +269,25 @@ if __name__ == "__main__":
     """
 
     while updater.is_alive():
+      print("-" * 80)
       for t in SyncStage(previous_commit, current_commit).tasklets():
-        print(time.time(), t.tid, ('startable', t.is_startable()), ('running', t.is_running()), ('done', t.is_finished()), end=' ')
+        pretty_print_status(t)
         if t.is_startable():
           if t.is_running():
-            print("running")
             continue
 
           if t.is_finished():
-            print("finished")
             continue
 
-          print_color("starting", color='green')
           updater.output = False
-          raw_input("run?")
+          raw_input("run (%s)? " % t.tid)
           updater.output = True
           def run(t=t):
             driver = libcloud_gae.new_driver()
             t.run(driver)
           threading.Thread(target=run).start()
-        else:
-          print("pending")
-
       print("-" * 80)
+
       time.sleep(1)
   finally:
     updater.go = False
