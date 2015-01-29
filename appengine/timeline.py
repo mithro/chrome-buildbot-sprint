cdc7589bba1eb5dca6e5601e9a70af4359f8d4d8
Alan Cutter <alancutter@chromium.org>
2015-01-29 15:04:55 +1100

    Limit the view period for the timeline

--- appengine/timeline.py
+++ appengine/timeline.py
@@ -1,4 +1,7 @@
-from datetime import datetime
+from datetime import (
+  datetime,
+  timedelta,
+)
 import webapp2
 
 from tasklet_time_log import (
@@ -7,21 +10,28 @@ from tasklet_time_log import (
 )
 
 
-def start_time_sorted(model):
-  return sorted(model.query(), cmp=lambda x,y: cmp(x.start_time, y.start_time))
+VIEW_HOURS = 4
+
+
+def start_time_sorted(query):
+  return sorted(query, cmp=lambda x,y: cmp(x.start_time, y.start_time))
 
 
 class TimelineHandler(webapp2.RequestHandler):
   def get(self):
-    now = datetime.now()
+    now = datetime.utcnow()
     self.response.write('Current time: %s\n' % now)
     self.response.write('\n\n')
     self.response.write('Running tasklets:\n')
-    for timer in start_time_sorted(TaskletTimer):
-      self.response.write(' - %s %s (%ds)\n' % (timer.start_time, timer.tid(), (now - timer.start_time).total_seconds()))
+    for timer in start_time_sorted(TaskletTimer.query()):
+      current_duration = (now - timer.start_time).total_seconds()
+      self.response.write(' - %s %s (%ds)\n' % (timer.start_time, timer.tid(), current_duration))
     self.response.write('\n\n')
-    self.response.write('Completed tasklets:\n')
-    for duration in start_time_sorted(TaskletDuration):
+    self.response.write('Completed tasklets in the last %s hours:\n' % VIEW_HOURS)
+    durations = TaskletDuration.query().filter(
+      TaskletDuration.stop_time >= now - timedelta(hours=VIEW_HOURS),
+    )
+    for duration in start_time_sorted(durations):
       self.response.write(' - %s %s (%ds)\n' % (duration.start_time, duration.tid, duration.seconds))
     self.response.headers.add_header('Content-Type', 'text/plain')
 
