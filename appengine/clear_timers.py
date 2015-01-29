da4f4fedae4f534321dd8ca442a26b8a45a066dc
Alan Cutter <alancutter@chromium.org>
2015-01-30 10:29:36 +1100

    Add timer clearer page for appengine

---
+++ appengine/clear_timers.py
@@ -0,0 +1,22 @@
+import webapp2
+
+from tasklet_time_log import TaskletTimer
+
+
+class ClearTimersHandler(webapp2.RequestHandler):
+  def get(self):
+    self.response.write('Clear timers?<br>')
+    for timer in TaskletTimer.query():
+      self.response.write(' - %s<br>' % timer.tid())
+    self.response.write('<form method="POST"><input type="submit"></input></form>')
+
+  def post(self):
+    self.response.write('Cleared timers:<br>')
+    for timer in TaskletTimer.query():
+      self.response.write(' - %s<br>' % timer.tid())
+      timer.key.delete()
+
+
+APP = webapp2.WSGIApplication([
+  ('/clear_timers/?', ClearTimersHandler),
+], debug=True)
