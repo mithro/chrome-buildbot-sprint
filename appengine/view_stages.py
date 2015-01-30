daefeaf9b0718c4822f10afcba6263d6ddc97ac0
Jack Hou <jackhou@google.com>
2015-01-30 14:11:14 +1100

    Neaten view_stages and stage_status

--- appengine/view_stages.py
+++ appengine/view_stages.py
@@ -12,14 +12,22 @@ class ViewStagesHandler(webapp2.RequestHandler):
   def get(self):
     self.response.out.write(
       TEMPLATE_STAGE.render(
-        stages=[
+        stages_in_progress=[
           stage
           for stage in get_current_stages()
           if not stage.is_finished() and any(
             tasklet.is_startable() or tasklet.is_running() or tasklet.is_finished()
             for tasklet in stage.tasklets
           )
-        ]
+        ],
+        stages_other=[
+          stage
+          for stage in get_current_stages()
+          if stage.is_finished() or not any(
+            tasklet.is_startable() or tasklet.is_running() or tasklet.is_finished()
+            for tasklet in stage.tasklets
+          )
+        ],
       )
     )
 
